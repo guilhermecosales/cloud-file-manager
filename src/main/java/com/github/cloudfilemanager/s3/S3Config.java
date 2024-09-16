7ac3cf1bf5242d4637788e4c68d6114799db4115
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-09-16 01:35:24 -0300

    feat(s3): add configuration for S3

---
+++ src/main/java/com/github/cloudfilemanager/s3/S3Config.java
@@ -0,0 +1,26 @@
+package com.github.cloudfilemanager.s3;
+
+import com.github.cloudfilemanager.configuration.CloudConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    private final CloudConfiguration cloudConfiguration;
+
+    @Bean
+    public S3Client s3Client() {
+        final Region region = Region.of(cloudConfiguration.getRegion());
+
+        return S3Client.builder()
+                .region(region)
+                .forcePathStyle(true)
+                .build();
+    }
+
+}
