d7e464684c6c64cbb21122615fbe7b522e467782
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-09-16 23:05:03 -0300

    feat(s3): add aws credentials

--- src/main/java/com/github/cloudfilemanager/configuration/CloudConfiguration.java
+++ src/main/java/com/github/cloudfilemanager/configuration/CloudConfiguration.java
@@ -3,6 +3,7 @@ package com.github.cloudfilemanager.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 
 @Data
 @Configuration("cloudConfiguration")
@@ -10,5 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class CloudConfiguration {
 
     private String region;
+
+    private String accessKey;
+
+    private String secretKey;
+
+    private String bucketName;
+
+    public AwsBasicCredentials getCredentialsProvider() {
+        return AwsBasicCredentials.create(accessKey, secretKey);
+    }
 
 }
--- src/main/java/com/github/cloudfilemanager/s3/S3Config.java
+++ src/main/java/com/github/cloudfilemanager/s3/S3Config.java
@@ -4,6 +4,7 @@ import com.github.cloudfilemanager.configuration.CloudConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -17,8 +18,11 @@ public class S3Config {
     public S3Client s3Client() {
         final Region region = Region.of(cloudConfiguration.getRegion());
 
+        AwsBasicCredentials awsBasicCredentials = cloudConfiguration.getCredentialsProvider();
+
         return S3Client.builder()
                 .region(region)
+                .credentialsProvider(() -> awsBasicCredentials)
                 .forcePathStyle(true)
                 .build();
     }
