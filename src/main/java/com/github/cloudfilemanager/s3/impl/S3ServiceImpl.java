06dea094a03cf4ce1aa5f9a86975c6d0af0d482b
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-10-02 16:15:22 -0300

    refactor(service): apply interface for service implementation

---
+++ src/main/java/com/github/cloudfilemanager/s3/impl/S3ServiceImpl.java
@@ -0,0 +1,97 @@
+package com.github.cloudfilemanager.s3.impl;
+
+import com.github.cloudfilemanager.configuration.CloudConfiguration;
+import com.github.cloudfilemanager.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class S3ServiceImpl implements S3Service {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final CloudConfiguration cloudConfiguration;
+
+    private final S3Client s3Client;
+
+    public void putObject(String key, byte[] file) {
+        String bucketName = cloudConfiguration.getBucketName();
+        log.info("Uploading file to S3 bucket: {}", bucketName);
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        RequestBody requestBody = RequestBody.fromBytes(file);
+
+        s3Client.putObject(objectRequest, requestBody);
+        log.info("File uploaded successfully to S3 bucket: {}", bucketName);
+    }
+
+    public byte[] getObject(String keyName) {
+        String bucketName = cloudConfiguration.getBucketName();
+        log.info("Downloading file from S3 bucket: {}", bucketName);
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+        ResponseInputStream<GetObjectResponse> response = s3Client.getObject(objectRequest);
+        try {
+            log.info("File downloaded successfully from S3 bucket: {}", bucketName);
+            return response.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generatePresignedUrl(String keyName) {
+        log.info("Generating presigned URL for file: {}", keyName);
+        try (S3Presigner presigner = S3Presigner.create()) {
+            String bucketName = cloudConfiguration.getBucketName();
+
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(keyName)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10))
+                    .getObjectRequest(objectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+
+            log.info("Presigned URL generated successfully for file: {}", keyName);
+
+            return presignedRequest.url().toExternalForm();
+        }
+    }
+
+    public void deleteObject(String key) {
+        String bucketName = cloudConfiguration.getBucketName();
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+
+}
--- src/main/java/com/github/cloudfilemanager/service/FileService.java
+++ src/main/java/com/github/cloudfilemanager/service/FileService.java
@@ -1,116 +1,25 @@
 package com.github.cloudfilemanager.service;
 
-import com.github.cloudfilemanager.configuration.CloudConfiguration;
 import com.github.cloudfilemanager.entity.FileEntity;
-import com.github.cloudfilemanager.exception.custom.FileNotFoundException;
-import com.github.cloudfilemanager.repository.FileRepository;
-import com.github.cloudfilemanager.s3.S3Service;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class FileService {
+public interface FileService {
 
-    private final Logger log = LoggerFactory.getLogger(FileService.class);
+    void uploadFile(MultipartFile file);
 
-    private final CloudConfiguration cloudConfiguration;
+    Page<FileEntity> getFilesMetadata(Pageable pageable);
 
-    private final FileRepository fileRepository;
+    List<FileEntity> getFileMetadataByName(@RequestParam(name = "fileName") String fileName);
 
-    private final S3Service s3Service;
+    byte[] downloadFile(String fileName);
 
-    public void uploadFile(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        log.info("Uploading file: {}", fileName);
+    String downloadFileUrl(String fileName);
 
-        try {
-            s3Service.putObject(fileName, file.getBytes());
-            saveMetadataFile(file);
-            log.info("File uploaded successfully.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void saveMetadataFile(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        double fileSize = file.getSize();
-        String mimeType = file.getContentType();
-
-        FileEntity newFile = FileEntity.builder()
-                .fileName(fileName)
-                .fileUrl(getS3Url(fileName))
-                .uploadDate(LocalDateTime.now())
-                .fileSize(fileSize)
-                .fileType(mimeType)
-                .build();
-
-        fileRepository.save(newFile);
-        log.info("File metadata saved successfully.");
-    }
-
-    private String getS3Url(String fileName) {
-        String bucketName = cloudConfiguration.getBucketName();
-
-        return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<FileEntity> getFilesMetadata(Pageable pageable) {
-        return fileRepository.findAll(pageable);
-    }
-
-    @Transactional(readOnly = true)
-    public List<FileEntity> getFileMetadataByName(@RequestParam(name = "fileName") String fileName) {
-        List<FileEntity> files = fileRepository.findByFileName(fileName);
-        if (files.isEmpty()) {
-            throw new FileNotFoundException(fileName);
-        }
-
-        return files;
-    }
-
-    public byte[] downloadFile(String fileName) {
-        log.info("Downloading file: {}", fileName);
-
-        FileEntity storedFile = getFileMetadataByExactName(fileName);
-
-        return s3Service.getObject(storedFile.getFileName());
-    }
-
-    public String downloadFileUrl(String fileName) {
-        FileEntity storedFile = getFileMetadataByExactName(fileName);
-
-        return s3Service.generatePresignedUrl(storedFile.getFileName());
-    }
-
-    @Transactional
-    public void deleteFile(String fileName) {
-        log.info("Deleting file: {}", fileName);
-
-        FileEntity storedFile = getFileMetadataByExactName(fileName);
-
-        s3Service.deleteObject(fileName);
-        fileRepository.deleteById(storedFile.getFileId());
-
-        log.info("File deleted successfully.");
-    }
-
-    private FileEntity getFileMetadataByExactName(String fileName) {
-        return fileRepository.findByFileNameIgnoreCase(fileName)
-                .orElseThrow(() -> new FileNotFoundException(fileName));
-    }
+    void deleteFile(String fileName);
 
 }
