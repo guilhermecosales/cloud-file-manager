e23d06772020ade2ad3dbd1f0741d47b6f67c786
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-10-19 22:21:24 -0300

    feat(password): encode password

--- src/main/java/com/github/cloudfilemanager/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/cloudfilemanager/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,9 +17,14 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public User saveUser(User user) {
         log.info("Saving user: {}", user);
+
+        user.setPassword(encodePassword(user.getPassword()));
+
         return userRepository.save(user);
     }
 
@@ -26,6 +32,10 @@ public class UserServiceImpl implements UserService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    private String encodePassword(final String password) {
+        return passwordEncoder.encode(password);
     }
 
 }
